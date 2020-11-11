daee553c82526e594f084a8520e8fdbd6aa560ef
Marco Mattion <ilmattion@outlook.com>
2020-11-11 22:28:52 +0100

    entità

---
+++ Tracker.DataAccess/Entities/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tracker.DataAccess.Entities
+{
+    public class Document
+    {
+        public int Identifier { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public int Size { get; set; }
+
+        public int UniqueIdentifierReference { get; set; }
+    }
+}
---
+++ Tracker.DataAccess/Entities/Process.cs
@@ -0,0 +1,13 @@
+﻿using Tracker.Models;
+
+namespace Tracker.DataAccess.Entities
+{
+    public class Process
+    {
+        public int Identifier { get; set; }
+
+        public ProcessType ProcessType { get; set; }
+
+        public int TimeSpent { get; set; }
+    }
+}
